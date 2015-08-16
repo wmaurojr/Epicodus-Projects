45bc0d87152546eeb41cd66826d89e1ab349cd56
wmaurojr <wmaurojr@gmail.com>
2015-08-16 10:43:29 -0400

    Instantiate class create 3 new objects assign to array

--- Object Practice/index.php
+++ Object Practice/index.php
@@ -17,7 +17,6 @@
 	
 class Car 
 {
- 	
  	function __construct($make_model, $price, $miles, $photo) {
 
 		$this->make = $make_model;
@@ -28,24 +27,11 @@ class Car
 	}
 }
 
-// $cars = array($murena, $rancho, $bagheera);
+$murena = new Car('1983 Matra Murena', 9000, 129000, 'img/matra-murena.jpg' );
+$rancho = new Car('1977 Matra Rancho', 14500, 179000, 'img/matra-rancho.jpg' );
+$bagheera = new Car('1979 Matra Bagheera', 19000, 68000, 'img/matra-bagheera.jpg' );
 
-?>
-
-<!-- 	<ul> -->
-		<?php /*
-			foreach ($cars as $car) {
-				echo "<li> $car->make_model</li>";
-				echo "<ul>";
-					echo "<li> $$car->price </li>";
-					echo " <li> Miles: $car->miles </li> ";
-				echo "</ul>";	
-			}			
-		?> */
-	// </ul>
-
-
-    ?>
+$cars = array($murena, $rancho, $bagheera);
 
 
     <!-- jQuery -->
