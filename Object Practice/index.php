a0ccda4bcc7b2b5c5adb57671a9cb89f33aa73ef
wmaurojr <wmaurojr@gmail.com>
2015-08-16 11:46:09 -0400

    Create first 'setter' for price property and test on object

--- Object Practice/index.php
+++ Object Practice/index.php
@@ -17,7 +17,7 @@
 	
 class Car 
 {
- 	function __construct($make_model, $price, $miles, $photo) {
+ 	function __construct($make_model, $price = 9000, $miles = 50000, $photo = 'img/matra-murena.jpg' ) {
 
 		$this->make = $make_model;
 		$this->price = $price;
@@ -25,11 +25,22 @@ class Car
 		$this->image = $photo;
 
 	}
+
+	function setPrice($new_price) {
+		$float_price = (float) $new_price;
+		if ($float_price != 0) {
+			$this->price = $float_price;
+		}
+	}
 }
+
 
 $murena = new Car('1983 Matra Murena', 9000, 129000, 'img/matra-murena.jpg' );
 $rancho = new Car('1977 Matra Rancho', 14500, 179000, 'img/matra-rancho.jpg' );
 $bagheera = new Car('1979 Matra Bagheera', 19000, 68000, 'img/matra-bagheera.jpg' );
+$bagheera->setPrice(24000.2924);
+
+
 
 $cars = array($murena, $rancho, $bagheera); ?>
 
