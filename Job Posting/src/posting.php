9af1823ecf1d824f2a125d2a0f0bfbb234d53a3f
wmaurojr <wmaurojr@gmail.com>
2015-08-30 23:16:53 -0400

    create Posting class

--- Job Posting/src/posting.php
+++ Job Posting/src/posting.php
@@ -1,5 +1,49 @@
 <?php 
 
-echo 'Hello!';
+	class Posting {
+
+		private $title;
+		private $description;
+		private $first_name;
+		private $last_name;
+		private $email;
+		private $phone;
+
+		function __construct($title, $description, $first_name, $last_name, $email, $phone) {
+
+			$this->title = $title;
+			$this->description = $description;
+			$this->first_name = $first_name;
+			$this->last_name = $last_name;
+			$this->email = $email;
+			$this->phone = $phone;
+		}
+
+		function getTitle(){
+			return $this->title;
+		}
+
+		function getDescription(){
+			return $this->description;
+		}
+
+		function getFirstName(){
+			return $this->first_name;
+		}
+
+		function getLastName(){
+			return $this->last_name;
+		}
+
+		function getEmail(){
+			return $this->email;
+		}
+
+		function getPhone(){
+			return $this->phone;
+		}
+
+	}
 
 ?>
+
